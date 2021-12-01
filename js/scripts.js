320d0f8821d24d1768f89872c497fcc3e7d8f619
jessinmacdon <jessinmacdon@gmail.com>
2021-12-01 19:08:26 +0100

    added pokemon objects to pokemonList
    - added 4 pokemon objects to pokerList

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-let pokemonList = [];
+let pokemonList = [
+  {name: 'mankey', height: 0.5, type: ['flying', 'grass']},
+  {name: 'abra', height: 0.9, type: ['ghost', 'fighting']},
+  {name:'meel', height: 1.1, type: ['electric', 'water']},
+  {name: 'mew', height: 0.4, type: ['dragon', 'psychic']}
+];
