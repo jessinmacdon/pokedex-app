a49ca9e270596f23f6542e965c9dc706f028f367
jessinmacdon <jessinmacdon@gmail.com>
2021-12-06 16:41:40 +0100

    iterating over items in pokemonList
    - Listing array items names and heights using loops and adding conditional to display string for the highest height value in the array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 let pokemonList = [
-  {name: 'mankey', height: 0.5, types: ['flying', 'grass']},
-  {name: 'abra', height: 0.9, types: ['ghost', 'fighting']},
-  {name:'meel', height: 1.1, types: ['electric', 'water']},
-  {name: 'mew', height: 0.4, types: ['dragon', 'psychic']}
+  {name: 'Mankey', height: 0.5, types: ['flying', 'grass']},
+  {name: 'Abra', height: 0.9, types: ['ghost', 'fighting']},
+  {name:'Meel', height: 1.1, types: ['electric', 'water']},
+  {name: 'Mew', height: 0.4, types: ['dragon', 'psychic']}
 ];
+
+//display as a list
+document.write('<ul>')
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write('<li>')
+  //display all array items .names and heights on DOM (using for loop)
+  document.write(pokemonList[i].name + " - Height: " + pokemonList[i].height);
+  // for the highest value for height display a string (using conditionals)
+  if (pokemonList[i].height > 1.0) document.write("  - Wow, thats tall!");
+  document.write("</li>");
+}
+document.write("</ul>");
