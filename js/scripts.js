1b467a3fa6b8fa3d4fd322490a894fd7f0acca98
jessinmacdon <jessinmacdon@gmail.com>
2021-12-10 12:31:04 +0100

    adding forEach loop
    - Swapping for loop for forEach loop
    - moving pokemonList object keys to individual lines

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,35 @@
 let pokemonList = [
-  {name: 'Mankey', height: 0.5, types: ['flying', 'grass']},
-  {name: 'Abra', height: 0.9, types: ['ghost', 'fighting']},
-  {name:'Meel', height: 1.1, types: ['electric', 'water']},
-  {name: 'Mew', height: 0.4, types: ['dragon', 'psychic']}
+  {
+    name: 'Mankey',
+    height: 0.5,
+    types: ['flying', 'grass']
+  },
+  {
+    name: 'Abra',
+    height: 0.9,
+    types: ['ghost', 'fighting']
+  },
+  {
+    name:'Meel',
+    height: 1.1,
+    types: ['electric', 'water']
+  },
+  {
+    name: 'Mew',
+    height: 0.4,
+    types: ['dragon', 'psychic']
+  }
 ];
 
 //display as a list
 document.write('<ul>')
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write('<li>')
-  //display all array items .names and heights on DOM (using for loop)
-  document.write(pokemonList[i].name + " - Height: " + pokemonList[i].height);
-  // for the highest value for height display a string (using conditionals)
-  if (pokemonList[i].height > 1.0) document.write("  - Wow, thats tall!");
-  document.write("</li>");
-}
+pokemonList.forEach(function(Pokemon) {
+  let pokemonName = pokemonList.name
+  let pokemonHeight = pokemonList.height
+  let pokemonTypes = pokemonList.types
+  document.write('<li>');
+  document.write(pokemonName + " - Height: " + pokemonHeight);
+ if (pokemonHeight > 1.7) document.write('  - Wow, thats tall!');
+ document.write("</li>");
+});
 document.write("</ul>");
