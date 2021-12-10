f38cc709923490884d7d2e23ce52cd6743d5bb12
jessinmacdon <jessinmacdon@gmail.com>
2021-12-10 14:01:34 +0100

    Updated file with IIFE
    - created IIFE returning different functions.
    - updated forEach loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,58 @@
-let pokemonList = [
-  {
-    name: 'Mankey',
-    height: 0.5,
-    types: ['flying', 'grass']
-  },
-  {
-    name: 'Abra',
-    height: 0.9,
-    types: ['ghost', 'fighting']
-  },
-  {
-    name:'Meel',
-    height: 1.1,
-    types: ['electric', 'water']
-  },
-  {
-    name: 'Mew',
-    height: 0.4,
-    types: ['dragon', 'psychic']
+//IIFE assigned to new variable
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Mankey',
+      height: 0.5,
+      types: ['flying', 'grass']
+    },
+    {
+      name: 'Abra',
+      height: 0.9,
+      types: ['ghost', 'fighting']
+    },
+    {
+      name:'Meel',
+      height: 1.1,
+      types: ['electric', 'water']
+    },
+    {
+      name: 'Mew',
+      height: 0.4,
+      types: ['dragon', 'psychic']
+    }
+  ];
+
+  //returning pokemon list
+  function getAll() {
+    return pokemonList;
   }
-];
+
+  // addiing new pokemnon (item) to array
+  function add(pokemon) {
+    pokemonList.push(pokemon)
+  }
+
+  return {
+    getAll : getAll,
+    add: add
+  };
+})();
+//end of iife
+
 
 //display as a list
 document.write('<ul>')
-pokemonList.forEach((item, i) => {
-  document.write('<li>')
-  //display all array items .names and heights on DOM (using for loop)
-  document.write(pokemonList[i].name + " - Height: " + pokemonList[i].height);
-  // for the highest value for height display a string (using conditionals)
-  if (pokemonList[i].height > 1.0) document.write("  - Wow, thats tall!");
+//adding getAll to forEach loop
+pokemonRepository.getAll().forEach(function(pokemon){
+  let pokemonName = pokemon.name
+  let pokemonHeight = pokemon.height
+  let pokemonTypes = pokemon.types
+  //print pkemon list
+  document.write('<li>');
+  document.write(pokemonName + " - Height: " + pokemonHeight);
+  //adding conditional
+  if (pokemonHeight > 1) document.write("  - Wow, thats tall!");
   document.write("</li>");
 });
+document.write("</ul>");
