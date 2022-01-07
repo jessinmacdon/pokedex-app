bcd376b4e032bba7bd554bce3404977a600da0f9
jessinmacdon <jessinmacdon@gmail.com>
2022-01-07 11:11:59 +0100

    updating UI on Pokemon App
    - adding types to modal on  js
    - adding style to app (modal)

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,14 @@ let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
   let modalContainer = document.querySelector('#modal-container');
+  const typeColors = {
+    'bug': '#a9b720', 'flying': '#a890f0', 'fire': '#f08030',
+    'psychic': '#f95887', 'normal': '#a7a877', 'water': '#6790f0', 'grass': '#78c84f',
+    'electric': '#f9cf30', 'ice': '#99d7d8', 'fighting': '#c13128',
+    'poison': '#9f409f', 'ground': '#e1bf68', 'rock': '#6f5848',
+    'ghost': '#705898', 'dark': '#6f5848', 'dragon': '#7138f8',
+    'steel': '#b8b8d0', 'fairy': '#efb5bc',
+  }
 
   //returning pokemon list
   function getAll() {
@@ -48,6 +56,7 @@ let pokemonRepository = (function () {
     })
   }
 
+
   function loadDetails(item) {
     let url = item.detailsUrl;
     return fetch(url).then(function (response) {
@@ -55,7 +64,7 @@ let pokemonRepository = (function () {
     }).then(function (details) {
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
-      item.types = details.types;
+      item.types = details.types.map(function(t) { return t.type.name })
     }).catch(function (e) {
       console.error(e);
     });
@@ -84,19 +93,37 @@ let pokemonRepository = (function () {
 
     //content title - display pokemon name
     let titleElement = document.createElement('h2');
+    titleElement.classList.add('modaltitle');
     titleElement.innerText = pokemon.name;
 
     //diplay height and image
-    let heightElement = document.createElement('p');
-    heightElement.innerText = 'Height: ' + pokemon.height;
+    let heightButtonElement = document.createElement('button');
+    heightButtonElement.classList.add('heightbtn');
+    heightButtonElement.innerText = 'Height: ' + pokemon.height;
 
     let imageElement = document.createElement('img');
     imageElement.src = pokemon.imageUrl;
 
     modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(heightElement);
     modal.appendChild(imageElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(heightButtonElement);
+
+
+    // here types is an array eg: ['fire', 'land']
+    pokemon.types.forEach(function(t) {
+      let typesElement = document.createElement('div');
+      typesElement.classList.add('typescontainer');
+
+      let typesButtonElement = document.createElement('button');
+      typesButtonElement.classList.add('typebtn')
+      typesButtonElement.style.backgroundColor = typeColors[t];
+      typesButtonElement.innerText = t;
+
+      typesElement.appendChild(typesButtonElement);
+      modal.appendChild(typesElement);
+    });
+
     modalContainer.appendChild(modal);
 
     modalContainer.classList.add('is-visible');
