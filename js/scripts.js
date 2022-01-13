414e467094ace4210580c454210d111879e43b90
jessinmacdon <jessinmacdon@gmail.com>
2022-01-13 16:14:16 +0100

    Creating dist folder
    - adding minified js
    - adding minified css
    - linking both files to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
 //IIFE assigned to new variable
-let pokemonRepository = (function () {
+let pokemonRepository = (function() {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
   let modal = document.querySelector('#pokedex');
   const typeColors = {
-    'bug': '#737d00', 'flying': '#835fdb', 'fire': '#ba5d00',
-    'psychic': '#eb0065', 'normal': '#797b00', 'water': '#2d73e2', 'grass': '#418600',
-    'electric': '#8e01fd', 'ice': '#008485', 'fighting': '#c13128',
-    'poison': '#9f409f', 'ground': '#654320', 'rock': '#6f5848',
-    'ghost': '#705898', 'dark': '#6f5848', 'dragon': '#7138f8',
-    'steel': '#73739a', 'fairy': '#bb5667',
-  }
-
+    bug: '#737d00',
+    flying: '#835fdb',
+    fire: '#ba5d00',
+    psychic: '#eb0065',
+    normal: '#797b00',
+    water: '#2d73e2',
+    grass: '#418600',
+    electric: '#8e01fd',
+    ice: '#008485',
+    fighting: '#c13128',
+    poison: '#9f409f',
+    ground: '#654320',
+    rock: '#6f5848',
+    ghost: '#705898',
+    dark: '#6f5848',
+    dragon: '#7138f8',
+    steel: '#73739a',
+    fairy: '#bb5667'
+  };
 
   //returning pokemon list
   function getAll() {
@@ -36,7 +47,6 @@ let pokemonRepository = (function () {
     button.innerText = pokemon.name;
     button.classList.add('button-class');
 
-
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
 
@@ -47,39 +57,46 @@ let pokemonRepository = (function () {
   }
 
   function loadList() {
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
-    }).then(function (json) {
-      json.results.forEach(function (item) {
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
+    return fetch(apiUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        json.results.forEach(function(item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+        });
+      })
+      .catch(function(e) {
+        console.error(e);
       });
-    }).catch(function (e) {
-      console.error(e);
-    })
   }
-
 
   function loadDetails(item) {
     let url = item.detailsUrl;
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.weight = details.weight;
-      item.types = details.types.map(function(t) { return t.type.name })
-    }).catch(function (e) {
-      console.error(e);
-    });
+    return fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(details) {
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.weight = details.weight;
+        item.types = details.types.map(function(t) {
+          return t.type.name;
+        });
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
   }
 
   //new function called on the eventlistener below (logs details from loadDetails fucntion)
   function showDetails(pokemon) {
-    loadDetails(pokemon).then(function () {
+    loadDetails(pokemon).then(function() {
       showModal(pokemon);
     });
   }
@@ -113,7 +130,7 @@ let pokemonRepository = (function () {
 
     pokemon.types.forEach(function(t) {
       let typesButtonElement = document.createElement('button');
-      typesButtonElement.classList.add('btn-info')
+      typesButtonElement.classList.add('btn-info');
       typesButtonElement.style.backgroundColor = typeColors[t];
       typesButtonElement.innerText = 'Type: ' + t;
 
@@ -128,32 +145,37 @@ let pokemonRepository = (function () {
   }
 
   // adding search to page
-  $(document).ready(function(){
+  $(document).ready(function() {
     $('#search-pokemon').on('input', function() {
-      var value = $(this).val().toLowerCase();
+      var value = $(this)
+        .val()
+        .toLowerCase();
       $('.group-list-item').filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        $(this).toggle(
+          $(this)
+            .text()
+            .toLowerCase()
+            .indexOf(value) > -1
+        );
       });
     });
   });
 
-
-
-return {
-  getAll: getAll,
-  add: add,
-  addListItem: addListItem,
-  loadList: loadList,
-  loadDetails: loadDetails,
-  showDetails: showDetails,
-  showModal: showModal
-};
+  return {
+    getAll: getAll,
+    add: add,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails,
+    showModal: showModal
+  };
 })();
 
 // IIFE ends here
 
 pokemonRepository.loadList().then(function() {
-  pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.getAll().forEach(function(pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
 });
